4c5f035410144c3e0c1fefc647bdd32f560a624b
Guy <guythomas721@gmail.com>
2019-09-05 08:41:00 -0700

    Add font style to footer

--- src/components/Gallery/FooterCaption/index.js
+++ src/components/Gallery/FooterCaption/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import './FooterCaption.css';
+
 export const FooterCaption = ({
   image: { art: { caption, height, width, frame, title } } = {},
 }) => {
   return (
-    <div className="index-footer">
+    <div className="footer-caption">
       {title && <h2>{title}</h2>}
       {caption && <h3>{caption}</h3>}
       {width && height && (
