99955925d8d7f3fc917f52649ca9734560ab7b5c
Guy <guythomas721@gmail.com>
2019-09-15 11:20:00 -0700

    Better

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,14 +7,14 @@ import './Header.css'
 
 export const Header = React.forwardRef(({ onChevronClick }, ref) => {
   return (
-    <header className="header-container">
+    <header className="header-sizing-container">
         <div className="header-contents">
           <h1 ref={ref}>Lettes Art</h1>
           <img src={hero} />
+          <button className="next-section-button" onClick={onChevronClick}>
+            <img src={downChevron} />
+          </button>
         </div>
-        <button className="next-section-button" onClick={onChevronClick}>
-          <img src={downChevron} />
-        </button>
     </header>
   )
 });
