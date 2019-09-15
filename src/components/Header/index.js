13b77c6bd331ec62a2e93b6f318305bef63dcbfb
Guy <guythomas721@gmail.com>
2019-09-15 13:20:12 -0700

    Change img to background image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,24 @@
 import React from 'react'
 
-import hero from 'images/hero.png';
-import downChevron from 'svg/down-chevron.svg';
+import hero from 'images/hero.png'
+import downChevron from 'svg/down-chevron.svg'
 
 import './Header.css'
 
 export const Header = React.forwardRef(({ onChevronClick }, ref) => {
   return (
     <header className="header-sizing-container">
-        <div className="header-contents">
-          <div className="header-wrapper">
-            <h1 ref={ref}>Lettes Art</h1>
-          </div>
-          <img src={hero} />
-          <button className="next-section-button" onClick={onChevronClick}>
-            <img src={downChevron} />
-          </button>
+      <div
+        className="header-contents"
+        style={{ backgroundImage: `url(${hero})` }}
+      >
+        <div className="header-wrapper">
+          <h1 ref={ref}>Lettes Art</h1>
         </div>
+        <button className="next-section-button" onClick={onChevronClick}>
+          <img src={downChevron} />
+        </button>
+      </div>
     </header>
   )
-});
+})
